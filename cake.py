951373748a9ba4eadc7cb20122653993fa1e4376
Rujolina <Liliaarelizetorres@gmail.com>
2021-03-24 11:41:34 -0400

    my work will be saved

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeorbust = input("Do you want cake? (Yes/No)")
+
+if cakeorbust == "yes"|| cakeorbust == "YES":
+        print("Have some cake!")
+elif cakeorbust == "no"|| cakeorbust === "NO":
+        print("No cake for you!")
+else:
+        print("Sorry, I don't understand.")
---
+++ spanish.py
@@ -0,0 +1,10 @@
+translator = input("Give me a word in English")
+
+if translator == "cat" or translator == "CAT":
+    print("gato")
+elif translator == "dog" or translator == "DOG":
+    print("perro")
+elif translator == "horse" or translator == "HORSE":
+    print("caballo")
+else:
+    print("no entiendo")
